850cd98b896d471bec22183d3388f51756627599
Michael Zaslavsky <mikezasl@gmail.com>
2020-10-10 01:18:58 -0500

    updated

--- Develop/index.js
+++ Develop/index.js
@@ -1,162 +1,91 @@
+const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+// file name
+const fn = "generated_README.md";
+
 // array of questions for user
 const questions = [
-    // this is for the title of the ReadME
     {
-        type: 'input',
-        name: 'title',
-        message: 'what is the title?'
+        type: "input",
+        message: "Project title?",
+        name: "title",
+        default: "Title"
     },
-
-    // to describe the product 
     {
-        type: 'input',
-        message: 'Please describe what the product is doing.',
-        name: 'description',
-
+        type: "input",
+        message: "Enter project description:",
+        name: "description",
+        default: "Description"
     },
-    // how to install this program
     {
-        type: 'input',
-        message: 'How do you install the program?',
-        name: 'install',
-
+        type: "input",
+        message: "How to installation instructions:",
+        name: "installation",
+        default: "Installation"
     },
-    // how would someone use the program? 
     {
-        type: 'input',
-        message: 'How would somone use the program',
-        name: 'usage',
-
+        type: "input",
+        message: "What is it used for?:",
+        name: "usage",
+        default: "Usage"
     },
-    // incase of issues where to contact you
     {
-        type: 'input',
-        message: 'Would you like you users to be able to report issues or contribute to this project? add instructions here.',
-        name: 'issues',
-
+        type: "input",
+        message: "Any contribution?:",
+        name: "contribution",
+        default: "Contribution"
     },
-    // what liscenes did you use or want to use 
     {
-        type: 'input',
-        message: 'what liscens did you use? put "none" if no lisecnes ',
-        name: 'license',
-
+        type: "input",
+        message: "How do yo test project",
+        name: "tests",
+        default: "Tests"
     },
-    //who else contributed
     {
-        type: 'input',
-        message: 'who else contributed to the program? if none  one write "none" ',
-        name: 'contributors',
-
+        type: "input",
+        message: "Enter email address:",
+        name: "email",
+        default: "email@email.com"
     },
-    // how to test the program
     {
-        type: 'input',
-        message: 'how would someone test the program?" ',
-        name: 'test',
-
+        type: "input",
+        message: "Enter GitHub username:",
+        name: "user",
+        default: "User"
     },
-    // how to reach if they have questions 
     {
-        type: 'input',
-        message: 'how do we contact if someone has questions?" ',
-        name: 'questions',
-
+        type: "input",
+        message: "Enter project repository name:",
+        name: "repo",
+        default: "Repository"
     },
-
+    {
+        type: "list",
+        message: "Select type of license:",
+        name: "license",
+        choices: ["Creative Commons: No Copyright","Creative Commons: Attribution","Creative Commons: Attribution-NonCommercial"]
+    }
 ];
 
-
-
-
-function init() {
-    inquirer.prompt(questions).then((response)=>{
-    
-    // make the first line for the ReadMe  using the title for the name
-    fs.appendFileSync("README.md", ("# " + response.title )+ '\n', function(err) { 
-    
-        if (err) { 
-        console.log(err)
-        }
-        else {
-        console.log("Success")
-        }
-    
-    })
-   
-    // adds the discription of the project that the users entered 
-    fs.appendFileSync("README.md", ( response.description ) + '\n', function(err) { 
-    
-        if (err) { 
-        console.log(err)
-        }
-        else {
-        console.log("Success")
-        }
-    
-    })
-    //this is where the installation will go
-    fs.appendFileSync("README.md", ("## Installation" + '\n' + response.install )+ '\n', function(err) { 
-    
-        if (err) { 
-        console.log(err)
-        }
-        else {
-        console.log("Success")
-        }
-    
-    })
-    // for usage 
-    fs.appendFileSync("README.md", ("## How to use the Application" + '\n' + response.usage)+ '\n', function(err) { 
-    
-        if (err) { 
-        console.log(err)
-        }
-        else {
-        console.log("Success")
-        }
-    
-    })
-    // for if there is an issue
-    fs.appendFileSync("README.md", ("## Issue Reporting and Contributing" + '\n' + response.issues)+ '\n', function(err) { 
-    
-        if (err) { 
-        console.log(err)
-        }
-        else {
-        console.log("Success")
-        }
-    
-    })
-    // for contributers 
-    fs.appendFileSync("README.md", ("## Other Contibuting Developers:" + '\n' + response.contributors)+ '\n', function(err) { 
-    
-        if (err) { 
-        console.log(err)
-        }
-        else {
-        console.log("Success")
-        }
-    
-    })
-    // creates a sub header for licences and adds what ever is typed in 
-    fs.appendFileSync("README.md", ("## Licence(s)" + '\n' + response.license)+ '\n', function(err) { 
-    
-        if (err) { 
-        console.log(err)
-        }
-        else {
-        console.log("Success")
-        }
-    
-    })
-    
-    })
+// function to write README file
+const writeToFile = (fileName, data) => {
+    if (data.license === "Creative Commons: No Copyright") {
+        data.licenseBadge = "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)";
+        data.licenseDesc = "CC0 1.0 - Use this however you want as long as it's legal.";
+    } else if (data.license === "Creative Commons: Attribution") {
+        data.licenseBadge = "[![License: CC BY 4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](https://creativecommons.org/licenses/by/4.0/)";
+        data.licenseDesc = "CC BY 4.0 - Use this however you want as long as it's legal, but give credit by providing a link to the license and indicating changes you made.";
+    } else  if (data.license === "Creative Commons: Attribution-NonCommercial") {
+        data.licenseBadge = "[![License: CC BY-NC 4.0](https://licensebuttons.net/l/by-nc/4.0/80x15.png)](https://creativecommons.org/licenses/by-nc/4.0/)";
+        data.licenseDesc = "CC BY-NC 4.0 - Use this however you want as long as it's legal and noncommercial, but give credit by providing a link to the license and indicating changes you made.";
     }
+    fs.writeFile(fileName, generateMarkdown(data), err => err ? console.log(err) : console.log(`Successfully created ${fn}`));
+}
 
+// function to initialize program
+const init = () => inquirer.prompt(questions).then(response => writeToFile(fn,response));
 
 // function call to initialize program
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,31 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+const generateMarkdown = data => 
+  `# ${data.title}
+  ## Description:
+  ${data.description}
+  
+  Link to repo: <https://github.com/${data.user}/${data.repo}>
+  ${data.licenseBadge}
+  ## Table of Contents:
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## License:
+  ${data.licenseDesc}
+  ## Contributing:
+  ${data.contribution}
+  ## Tests:
+  ${data.tests}
+  ## Questions:
+  * Feel free to contact me with any questions at [${data.email}](mailto:${data.email}).
+  * You can also check out my [GitHub Profile](https://github.com/${data.user}).
 `;
-}
 
 module.exports = generateMarkdown;
